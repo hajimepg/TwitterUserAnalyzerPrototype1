fcb8377b7f139432dadffed5c3a233646c4a7191
Kenichi Saito <hajime@studiohff.net>
2017-11-11 18:05:55 +0900

    画像を1個ずつ順番にダウンロード(Twitterに負荷を掛けないように最大10個

--- imageDownload.ts
+++ imageDownload.ts
@@ -3,9 +3,11 @@
 import { Buffer } from "buffer";
 import * as fs from "fs";
 import * as http from "http";
+import { setImmediate } from "timers";
 import * as url from "url";
 
 import * as Commander from "commander";
+import * as lodash from "lodash";
 
 import ExportFormat from "./exportFormat";
 import User from "./user";
@@ -29,32 +31,49 @@ try {
     const input = fs.readFileSync(Commander.input, { encoding: "utf8" });
     const data: ExportFormat = JSON.parse(input);
 
-    const screenName: string = data.followers[0].screen_name;
-    const imageUrl: string = data.followers[0].profile_image_url;
-    console.log(imageUrl);
-    http.get(imageUrl, (res) => {
-        if (res.statusCode !== 200) {
+    let downloadCount: number = 0;
+    const downloadQueue = lodash.cloneDeep(
+        data.followers.slice(0, 10) // TODO: デバッグが終わったらsliceを外す
+    );
+    const download = () => {
+        if (downloadQueue.length === 0) {
             return;
         }
 
-        const contentType = res.headers["content-type"];
-        const extension: string = getExtension(contentType);
-        if (extension === "") {
-            console.log(`Unsupported content-type: ${contentType}`);
-            return;
-        }
+        const target = downloadQueue.shift();
+        const screenName: string = target.screen_name;
+        const imageUrl: string = target.profile_image_url;
+        console.log(imageUrl);
+        http.get(imageUrl, (res) => {
+            downloadCount++;
+            if (res.statusCode !== 200) {
+                console.log(`download ${imageUrl }${res.statusCode}`);
+                setImmediate(download);
+                return;
+            }
 
-        const chunks: Buffer[] = [];
-        res.on("data", (chunk: Buffer) => {
-            console.log("chunk received");
-            chunks.push(chunk);
+            const contentType = res.headers["content-type"];
+            const extension: string = getExtension(contentType);
+            if (extension === "") {
+                console.log(`Unsupported content-type: ${contentType}`);
+                setImmediate(download);
+                return;
+            }
+
+            const chunks: Buffer[] = [];
+            res.on("data", (chunk: Buffer) => {
+                console.log("chunk received");
+                chunks.push(chunk);
+            });
+            res.on("end", () => {
+                console.log("all data received");
+                const imageData = Buffer.concat(chunks);
+                fs.writeFileSync(`${screenName}.${extension}`, imageData);
+                setImmediate(download);
+            });
         });
-        res.on("end", () => {
-            console.log("all data received");
-            const imageData = Buffer.concat(chunks);
-            fs.writeFileSync(`${screenName}.${extension}`, imageData);
-        });
-    });
+    };
+    download();
 }
 catch (e) {
     console.error(e);
